ad7d5a9267dd62c299e6925ba01661a293dc781e
graham@WSL <@>
2022-12-07 14:48:51 -0700

    getting each yard

--- scrape/scraper.js
+++ scrape/scraper.js
@@ -1,13 +1,25 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
+const { db } = require('pg');
+const cheerio = require('cheerio');
 
-//Use selenium to open chrome and select the "TWIN FALLS" yard on JJ website
-(async function example() {
+
+(async function scrapeJalopyJungle() {
+  var makes, models;
   let driver = await new Builder().forBrowser('chrome').build();
   try {
     await driver.get('http://inventory.pickapartjalopyjungle.com');
-    yardField = driver.findElement(By.id('yard-id'));
-    await yardField.sendKeys('T');
-    await yardField.sendKeys(Key.TAB);
+    const yardHtml = await driver.findElement(By.xpath('//*[@id="yard-id"]')).getAttribute("innerHTML");
+    console.debug('scraping yards:');
+    console.debug(yardHtml);
+
+    // extract the name of each yard
+    let $ = cheerio.load(yardHtml);
+    let yards = [];
+    $("option").each(function () {
+      if ($(this).attr("value") != "") { yards.push($(this).text()); }
+    });
+
   } finally {
+    driver.close();
   }
 })();
