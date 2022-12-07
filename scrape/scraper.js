2ac42ef371fcc42efb8fb3df09c19c919e1b7d6a
graham@WSL <@>
2022-12-07 16:17:22 -0700

    playing with new iterate method

--- scrape/scraper.js
+++ scrape/scraper.js
@@ -4,22 +4,44 @@ const cheerio = require('cheerio');
 
 
 (async function scrapeJalopyJungle() {
-  var makes, models;
   let driver = await new Builder().forBrowser('chrome').build();
+  const yardsXpath = '//*[@id="yard-id"]';
   try {
     await driver.get('http://inventory.pickapartjalopyjungle.com');
-    const yardHtml = await driver.findElement(By.xpath('//*[@id="yard-id"]')).getAttribute("innerHTML");
-    console.debug('scraping yards:');
-    console.debug(yardHtml);
+    // zoom to 80%  
+    driver.executeScript("document.body.style.zoom='80%'");
 
-    // extract the name of each yard
-    let $ = cheerio.load(yardHtml);
-    let yards = [];
-    $("option").each(function () {
-      if ($(this).attr("value") != "") { yards.push($(this).text()); }
-    });
+    // const yardHtml = await driver.findElement(By.xpath(yardsXpath)).getAttribute("innerHTML");
+    // console.debug('scraping yards:');
+    // console.debug(yardHtml);
+
+    // // extract the name of each yard
+    // let $ = cheerio.load(yardHtml);
+    // let yards = [];
+    // $("option").each(function () {
+    //   if ($(this).attr("value") != "") { yards.push($(this).text()); }
+    // });
+
+    // // view each record in each yard
+    // await driver.findElement(By.xpath(yardsXpath));
+
+    let yardsParent = await driver.findElement(By.xpath(yardsXpath));
+    let yards = await yardsParent.findElements(By.tagName('option'));
+    for (let yard of yards) {
+      await driver.actions()
+        .move({ origin: yardsParent })
+        .pause(1000)
+        .click()
+        .perform();
+    }
 
   } finally {
     driver.close();
   }
 })();
+
+
+// dummy function - will connect to postgres
+function addRecord(yard, year, make, model, row) {
+
+}
